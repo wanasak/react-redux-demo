33a7e02091afacda85a0df6efd0f242807961085
Wanasak Suraintaranggoon <u510610433@gmail.com>
2019-06-25 17:58:58 +0700

    add courses page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import Header from './components/common/Header';
 import PageNotFound from './components/PageNotFound';
+import CoursesPage from './components/courses/CoursesPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CoursesPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Courese</h2>
+        <p>This is courses page</p>
+      </div>
+    );
+  }
+}
